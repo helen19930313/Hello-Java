a0984880b68bdd7915dfcf645f031b889f56a49e
NTUE <NTUE@10.0.1.52>
2017-11-22 11:36:47 +0800

    Hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
-
+//110428003
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World");
+		System.out.println("Hello, Java World");
 	}
 
 }
